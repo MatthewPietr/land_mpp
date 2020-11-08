e6bdb6940e3cb322d436ef377b788157b5331357
MatthewPietr <pietruchamatthew@gmail.com>
2020-11-08 15:55:20 -0600

    Initial Commit

---
+++ land_mpp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  land_mpp
+//
+//  Created by Matthew Pietrucha on 11/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ land_mpp/land_mppApp.swift
@@ -0,0 +1,17 @@
+//
+//  land_mppApp.swift
+//  land_mpp
+//
+//  Created by Matthew Pietrucha on 11/8/20.
+//
+
+import SwiftUI
+
+@main
+struct land_mppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
