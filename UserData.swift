2d06ff8cd1074e3a37ada0a0e1ebe66e4bb9ba37
MatthewPietr <mxp18b@acu.edu>
2020-11-10 08:23:49 -0600

    part 3 part 5

---
+++ UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  land_mpp
+//
+//  Created by Matthew Pietrucha on 11/10/20.
+//
+
+import SwiftUI
+
+import Combine
+
+final class UserData: ObservableObject  {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+}
